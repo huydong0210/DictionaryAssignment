73551023024f81158b0e44cb8e0fba8ce47e1b1c
Bui Huy Dong <huydong0210@gmail.com>
2021-10-06 16:23:37 +0700

    dinh dang word

---
+++ src/Word.java
@@ -0,0 +1,18 @@
+public class Word {
+    private String word_target; // English
+    private String word_explain;//Vietnamese
+
+    public String getWord_target(){
+        return this.word_target;
+    }
+    public void setWord_target(String word_target){
+        this.word_target=word_target;
+    }
+
+    public String getWord_explain(){
+        return this.word_explain;
+    }
+    public void setWord_explain(String word_explain){
+        this.word_explain=word_explain;
+    }
+}

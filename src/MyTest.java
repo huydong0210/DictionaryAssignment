dda8d9e0d2f406924ddc83e3f322be0b54059858
Bui Huy Dong <huydong0210@gmail.com>
2021-10-10 20:21:39 +0700

    Test

--- src/MyTest.java
+++ src/MyTest.java
@@ -2,9 +2,7 @@ import java.util.Locale;
 
 public class MyTest {
     public static void main(String[] args) {
-        Dictionary dictionary=new Dictionary();
-        DictionaryManagement.insertFromFile(dictionary);
-        DictionaryCommandline.dictionaryBasic(dictionary);
+        Pronounce.read("database");
 
     }
 }
